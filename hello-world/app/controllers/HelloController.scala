f77c6f933643f81a5430bf5813dbfd16b0a55955
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-05-07 18:20:55 +0900

    アプリケーションを国際化

--- hello-world/app/controllers/HelloController.scala
+++ hello-world/app/controllers/HelloController.scala
@@ -1,22 +1,20 @@
 package controllers
 
 import javax.inject.Inject
-import javax.inject.Singleton
-import play.api.mvc.AbstractController
+import play.api.i18n.I18nSupport
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
 import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
+import play.api.mvc.Controller
 
-@Singleton
-class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HelloController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def get(name: Option[String]) =
-    Action { implicit request: Request[AnyContent] =>
+    Action { implicit request =>
       Ok {
         name
-          .map(s => s"Hello, $s!")
-          .getOrElse("""Please give a name as a query parameter named "name".""")
+          .map(s => Messages("hello", s))
+          .getOrElse(Messages("noQuery"))
       }
     }
 }
